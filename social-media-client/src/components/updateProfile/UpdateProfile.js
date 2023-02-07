ebec7b3bdcd633a1fe6d9babc3817d4ef06463d6
Hari Om <harrypjhercules@gmail.com>
2023-02-07 18:12:17 +0530

    first commit

---
+++ social-media-client/src/components/updateProfile/UpdateProfile.js
@@ -0,0 +1,114 @@
+import React, { useEffect, useState } from "react";
+import "./UpdateProfile.scss";
+import "./UpdateProfile.scss";
+import dummyUserImg from "../../assets/user.png";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  deleteAcc,
+  setLoading,
+  updateMyProfile,
+} from "../../redux/slices/appConfigSlice";
+import { useNavigate } from "react-router-dom";
+import { KEY_ACCESS_TOKEN, removeItem } from "../../utils/localStorageManager";
+import { axiosClient } from "../../utils/axiosClient";
+
+function UpdateProfile() {
+  const myProfile = useSelector((state) => state.appConfigReducer.myProfile);
+  const navigate = useNavigate();
+  const [name, setName] = useState("");
+  const [bio, setBio] = useState("");
+  const [userImg, setUserImg] = useState("");
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    setName(myProfile?.name || "");
+    setBio(myProfile?.bio || "");
+    setUserImg(myProfile?.avatar?.url);
+  }, [myProfile]);
+
+  function handleImageChange(e) {
+    const file = e.target.files[0];
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(file);
+    fileReader.onload = () => {
+      if (fileReader.readyState === fileReader.DONE) {
+        setUserImg(fileReader.result);
+        console.log("img data", fileReader.result);
+      }
+    };
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    dispatch(
+      updateMyProfile({
+        name,
+        bio,
+        userImg,
+      })
+    );
+  }
+
+  async function handelDeleteAcc(e) {
+    try {
+      const response = await axiosClient.delete("/user/");
+      removeItem(KEY_ACCESS_TOKEN);
+      navigate("/login");
+      console.log("myResponse" + response);
+      return response.result;
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  }
+
+  return (
+    <div className="UpdateProfile">
+      <div className="container">
+        <div className="left-part">
+          <div className="input-user-img">
+            <label htmlFor="inputImg" className="labelImg">
+              <img src={userImg ? userImg : dummyUserImg} alt={name} />
+            </label>
+            <input
+              className="inputImg"
+              id="inputImg"
+              type="file"
+              accept="image/*"
+              onChange={handleImageChange}
+            />
+          </div>
+        </div>
+        <div className="right-part">
+          <form onSubmit={handleSubmit}>
+            <input
+              value={name}
+              type="text"
+              placeholder="Your Name"
+              onChange={(e) => setName(e.target.value)}
+            />
+            <input
+              value={bio}
+              type="text"
+              placeholder="Your Bio"
+              onChange={(e) => setBio(e.target.value)}
+            />
+            <input
+              type="submit"
+              className="btn-primary"
+              onClick={handleSubmit}
+            />
+          </form>
+
+          <button
+            className="delete-account btn-primary"
+            onClick={handelDeleteAcc}
+          >
+            Delete Account
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default UpdateProfile;
